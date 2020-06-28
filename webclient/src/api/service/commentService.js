69eb053101b625b184e868842cd0f8c874a8d64b
Ondersahinn <ondershin@gmail.com>
2020-06-28 18:01:56 +0300

    Add aws api routes

--- webclient/src/api/service/commentService.js
+++ webclient/src/api/service/commentService.js
@@ -4,14 +4,14 @@ import headers from '../../constans/header.json';
 import { parseResult } from './dataParser';
 
 export const createComment = async data => {
-  const url = `${'http://localhost:5001/routes/api/v1/comment'}`;
+  const url = `${'http://ec2-107-20-127-176.compute-1.amazonaws.com:3000/routes/api/v1/comment'}`;
   const config = headers.content_type.application_json;
   const result = await postRequest(url, data, config);
   const parsedResult = parseResult(result);
   return parsedResult;
 };
 export const updateComment = async (commentId, data) => {
-  const url = `${'http://localhost:5001/routes/api/v1/comment'}/${commentId}`;
+  const url = `${'http://ec2-107-20-127-176.compute-1.amazonaws.com:3000/routes/api/v1/comment'}/${commentId}`;
   const config = headers.content_type.application_json;
   const result = await putRequest(url, data, config);
   const parsedResult = parseResult(result);
@@ -19,7 +19,7 @@ export const updateComment = async (commentId, data) => {
 };
 
 export const deleteComment = async id => {
-  const url = `${'http://localhost:5001/routes/api/v1/comment'}/${id}`;
+  const url = `${'http://ec2-107-20-127-176.compute-1.amazonaws.com:3000/routes/api/v1/comment'}/${id}`;
   const config = headers.content_type.application_json;
   const result = await deleteRequest(url, config);
 
@@ -28,7 +28,7 @@ export const deleteComment = async id => {
 };
 
 export const searchAllComments = async () => {
-  const url = 'http://localhost:5001/routes/api/v1/comment';
+  const url = 'http://ec2-107-20-127-176.compute-1.amazonaws.com:3000/routes/api/v1/comment';
   const config = headers.content_type.application_json;
   const result = await getRequest(url, config);
   const parsedResult = parseResult(result);
@@ -36,7 +36,7 @@ export const searchAllComments = async () => {
 };
 
 export const searchCommentById = async id => {
-  const url = `${'http://localhost:5001/routes/api/v1/comment'}/${id}`;
+  const url = `${'http://ec2-107-20-127-176.compute-1.amazonaws.com:3000/routes/api/v1/comment'}/${id}`;
   const config = headers.content_type.application_json;
   const result = await getRequest(url, config);
   const parsedResult = parseResult(result);
